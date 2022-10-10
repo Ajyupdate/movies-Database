f24f94720a03914f6c04cf950eed5210495427a6
Ajibade <ajibadeemmanuel58@gmail.com>
2022-10-10 19:44:04 +0100

    head and everything

--- pages/_app.js
+++ pages/_app.js
@@ -5,21 +5,10 @@ export default function App({ Component, pageProps}){
     
     return (
         <>
-        {/* <Head>
+        <Head>
             <title>Movie Database</title>
-            <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-            <script src="https://unpkg.com/aos@next/dist/aos.js"></script> 
-      <script> */}
-        {/* {AOS.init(
-            {
-                offset:10,
-                duration:3000,
-                once: true
-            }
-        )} */}
-      {/* </script>
-
-        </Head> */}
+            
+        </Head> 
         <Component {...pageProps}/>
         </>
     )
